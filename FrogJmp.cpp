d914af81ffca72038205f1c0d1fbc0e7d2c0ce4d
Szabolcs Novaczki <novaczkisz@gmail.com>
2015-05-29 15:38:13 +0200

    final solution
    Passed 100% on codility.

--- FrogJmp.cpp
+++ FrogJmp.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
+int solution(int X, int Y, int D) {
+    int d = Y - X;
+
+    if( !d ) return 0;
+
+    if( D >= d) return 1;
+
+    if( !(d % D) ) return d / D;
+
+    return d / D + 1;
+}
+
+
 int main(){
 
-  std::cout << "Works!" << std::endl;
+  std::cout << "Test #1 (1,1,1) returned: " <<
+    solution( 1,1,1 ) <<
+    std::endl;
 
+  std::cout << "Test #1 (1,1,1) returned: " <<
+    solution( 1,3,1 ) <<
+    std::endl;
+
+  std::cout << "Test #1 (1,1,1) returned: " <<
+    solution( 1,5,6 ) <<
+    std::endl;
+  
   return 0;
 
 }
